610079a3c162761e85ea4cf367998441d7d28bbe
DILIP PRAJAPATI <dkprajapati9046@gmail.com>
2024-04-11 20:51:12 +0530

    added basic server app source code

---
+++ ServerApp/ServerApp/src/test/java/com/serverApp/ServerApp/ServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverApp.ServerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
